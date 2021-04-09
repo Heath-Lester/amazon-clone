d6362873630c9110f2974d8a8444d2a007c8dcd4
Heath Lester <h.allen.lester@gmail.com>
2021-04-09 14:02:25 -0500

    removed react initial files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCLLkGUCjgaERzNcpG4Y9CByTaN-D4jnKc",
+    authDomain: "challenge-60876.firebaseapp.com",
+    projectId: "challenge-60876",
+    storageBucket: "challenge-60876.appspot.com",
+    messagingSenderId: "929539474141",
+    appId: "1:929539474141:web:b99bcc7780bd64232c1134",
+    measurementId: "G-PKTKQ08GQ7"
+  };
