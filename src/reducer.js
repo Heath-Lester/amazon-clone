e5b048dd7fe9cceb930879afd31777332df6897d
Heath Lester <h.allen.lester@gmail.com>
2021-05-17 16:32:46 -0500

    finished user authentication

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,7 @@
+
 export const initialState = {
     basket: [],
+    user: null
 }
 
 export const getBasketTotal = (basket) =>
@@ -31,6 +33,12 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 basket: newBasket
+            }
+
+        case "SET_USER":
+            return {
+                ...state,
+                user: action.user
             }
 
         default:
