666ecbcce8780b2836a2bdaef6c54f59586dd01e
Heath Lester <h.allen.lester@gmail.com>
2021-05-27 02:13:25 -0500

    changed axios endpoint

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:5001/challenge-60876/us-central1/api'
+    baseURL: 'https://us-central1-challenge-60876.cloudfunctions.net/api'
+    // baseURL: 'http://127.0.0.1:5001/challenge-60876/us-central1/api'
 })
 
 
